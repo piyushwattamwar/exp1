25969fe767493dffdb889e73b56574e4542b3e32
piyushwattamwar <147603359+piyushwattamwar@users.noreply.github.com>
2023-10-11 15:28:03 +0530

    Create avgwithoutloop.cpp

---
+++ avgwithoutloop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a,b,c,d,e,sum,avg;
+    cout<<"Enter the numbers to find the average"<<endl;
+    cin>>a>>b>>c>>d>>e;
+    sum=a+b+c+d+e;
+    avg=sum/5;
+    cout<<"The result is"<<avg;
+    return 0;
+}
